f71bb99f2339f6ee75babc0358f9e385e79b296b
Karol S <karol96266@gmail.com>
2024-01-16 12:53:56 +0000

    Add check if req user is req owner in profile/serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -3,9 +3,14 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'name', 'image','total_tasks','total_collections','finished_tasks','current_goals'
+            'id', 'owner', 'created_at', 'name', 'image','total_tasks','total_collections','finished_tasks','current_goals','is_owner'
         ]
