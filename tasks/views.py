8b87a375a9a26bb41157dcc7117119077bdc02f5
Karol S <karol96266@gmail.com>
2024-01-17 12:57:55 +0000

    Add task detail view with edit/delete functionality

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
-
+from drf_api.permissions import IsOwnerOrReadOnly
 class TaskList(APIView):
     serializer_class = TaskSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
@@ -20,5 +20,40 @@ class TaskList(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetail(APIView):
+    serializer_class = TaskSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+
+    def get_object(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+            self.check_object_permissions(self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, context= {'request': request}
+        )
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        task = self.get_object(pk)
+        serializer = TaskSerializer(
+            task, data=request.data, context={'request':request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
         
 # Create your views here.
