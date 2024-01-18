8de33ec3d5a021bc5230833813a9586d767f1de3
Karol S <karol96266@gmail.com>
2024-01-18 16:03:00 +0000

    Add collectiondetail view delete method

--- task_collections/views.py
+++ task_collections/views.py
@@ -54,5 +54,11 @@ class TaskCollectionDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
 
 
