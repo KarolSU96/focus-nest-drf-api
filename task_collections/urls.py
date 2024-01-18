e100f1a0e47caf821c9610a2114587439248ccae
Karol S <karol96266@gmail.com>
2024-01-18 13:49:36 +0000

    Add collections to main urls

--- task_collections/urls.py
+++ task_collections/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from task_collection import views
+from task_collections import views
 
 urlpatterns= [
     path('task_collections/', view.TaskCollectionList.as_view()),
