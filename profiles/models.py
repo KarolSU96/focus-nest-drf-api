df39d865007b645f0b5e63d17bdf3f309aac9931
Karol S <karol96266@gmail.com>
2024-01-22 11:26:19 +0000

    Add logic for hadning task /colelction counter with signals

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from task_collections.models import TaskCollection
+from tasks.models import Task
 
 class Profile(models.Model):
 
@@ -23,6 +26,39 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner.username}'s profile"
+
+@receiver(post_save, sender=Task)
+def update_profile_total_tasks(sender, instance, created, **kwargs):
+    if created:
+        instance.owner.profile.total_tasks += 1
+        if instance.is_done:
+            instance.owner.profile.finished_tasks += 1
+        instance.owner.profile.save()
+    else:
+        # Task is being updated, check if is_done has changed
+        if instance.is_done and not instance.owner.profile.finished_tasks:
+            instance.owner.profile.finished_tasks += 1
+        elif not instance.is_done and instance.owner.profile.finished_tasks:
+            instance.owner.profile.finished_tasks -= 1
+        instance.owner.profile.save()  
+
+@receiver(post_save, sender=TaskCollection)
+def update_profile_total_collections(sender, instance, created, **kwargs):
+    if created:
+        instance.owner.profile.total_collections += 1
+        instance.owner.profile.save()
+
+@receiver(post_delete, sender=Task)
+def update_profile_total_tasks_on_delete(sender, instance, **kwargs):
+    instance.owner.profile.total_tasks -= 1
+    instance.owner.profile.save()
+
+
+@receiver(post_delete, sender=TaskCollection)
+def update_profile_total_collections_on_delete(sender, instance, **kwargs):
+    instance.owner.profile.total_collections -= 1
+    instance.owner.profile.save()
+
 
 # Signal handler to create a profile for a user when a new user is created
 def create_profile(sender, instance, created, **kwargs):
