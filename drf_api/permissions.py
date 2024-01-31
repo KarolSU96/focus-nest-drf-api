c87578eac27e06140168b028f28ccf69b5920ea6
Karol S <karol96266@gmail.com>
2024-01-31 10:00:06 +0000

    Fix formatting main app

--- drf_api/permissions.py
+++ drf_api/permissions.py
@@ -2,17 +2,16 @@ from rest_framework import permissions
 
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
-  """
-  Custom permission to allow only owners of an object to edit it.
-  Read-only permissions are allowed for any user.
-  """
-
-  def has_object_permission(self, request, view, obj):
     """
+    Custom permission to allow only owners of an object to edit it.
+    Read-only permissions are allowed for any user.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
         Check if the requesting user has permission to perform the requested action on the object.
-    """
-    # Allow read-only access for safe methods (GET, HEAD, OPTIONS)
-    if request.method in permissions.SAFE_METHODS:
-      return True
-    return obj.owner == request.user
-
+        """
+        # Allow read-only access for safe methods (GET, HEAD, OPTIONS)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
